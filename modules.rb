964862ee4c8c8c3d3aacf809e32c48f22c54cbf4
tanmaym150 <65558041+tanmaym150@users.noreply.github.com>
2022-10-21 12:20:56 +0530

    Add files via upload

---
+++ modules.rb
@@ -0,0 +1,19 @@
+#Modules are collection of methods,constants and class variables
+
+module GFG
+
+	c = 10;
+
+	def GFG.welcome
+		puts "Welcome to my module"
+	end
+	
+	def GFG.tracker
+		puts "This is my ruby tracker"
+	end
+end
+
+
+
+GFG.welcome
+GFG.tracker		
